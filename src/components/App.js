42dc16d099f9fb89fb2b2712b44ceb52ea78fae1
Duane <duanerichard47@hotmail.com>
2023-02-05 18:27:08 -0500

    copmplete

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
   // const appClass = false ? "App dark" : "App light"
-const[isOn, setIsOn]= useState(true)
+const[isOn, setIsOn]= useState(false)
 
   function handleClick(){
     setIsOn(!isOn)
